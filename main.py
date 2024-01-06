18a881e0e77a51ff8670b4265115940b2baf7db6
Kasino <kasinodev@gmail.com>
2024-01-06 04:42:19 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
-    print("Hello, World!")
+    album = 'With That Being Said'
+    artist = 'LeukoLeGoat'
+    release_year = '2024'
+
+    print(album)
+    print(artist)
+    print(release_year)
 
 
 if __name__ == '__main__':
     main()
-
-# Hello, World!
